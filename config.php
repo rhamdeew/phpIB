b5e8c8f638894c11052ad7686df7b32a52f40319
Rail Hamdeew <rail@hamdeew.ru>
2014-09-19 10:12:48 +0400

    split files into exec & config

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$mysql_user = "backuper";
+$mysql_password = "";
+$user_path = "/var/www/";
+$date = date("Y-m-d");
+$backupName = 'backup-'.$date;
+$backupsStorage = '/var/backups/local/';
+$ftpUser = '';
+$ftpPassword = '';
+$ftpHost = 'ftp.selcdn.ru';
+$ftpPath = 'test';
--- phpIB.php
+++ phpIB.php
@@ -1,15 +1,6 @@
 <?php
 
-$mysql_user = "backuper";
-$mysql_password = "";
-$user_path = "/var/www/";
-$date = date("Y-m-d");
-$backupName = 'backup-'.$date;
-$backupsStorage = '/var/backups/local/';
-$ftpUser = '';
-$ftpPassword = '';
-$ftpHost = 'ftp.selcdn.ru';
-$ftpPath = 'test';
+require "config.php";
 
 //Getting list of databases
 
